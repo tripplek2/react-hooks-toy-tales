cd6aca4f72293760ce202fc1559a05c8fd0f20c9
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-12 20:48:49 +0300

    receiving toys as props,looping  through them and rendering card

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys }) {
   return (
     <div id="toy-collection">
       {ViewTransitionTypeSet.map((toy) => (
