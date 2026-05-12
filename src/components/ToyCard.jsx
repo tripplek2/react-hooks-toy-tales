f95d16263f085ecab8c9ba85f32d15d32a33c59e
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-12 22:26:42 +0300

    added function to handle delete

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-function ToyCard({ toy }) {
+function ToyCard({ toy, deleteToy }) {
+  function handleDelete() {
+    fetch(`${toy,id}`, {
+      method: "DELETE",
+    })
+    .then(()=> {
+      dleteToy(toy.id)
+    });
+  }
   return (
     <div className="card" data-testid="toy-card">
       <h2>{toy.name}</h2>
@@ -11,7 +19,7 @@ function ToyCard({ toy }) {
       />
       <p>{toy.likes} Likes </p>
       <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <button className="del-btn" onClick={handleDelete}>Donate to GoodWill</button>
     </div>
   );
 }
