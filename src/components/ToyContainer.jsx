115cbf8894a2e834c05c148a52bc31cce732d14c
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-13 15:22:40 +0300

    defined deleteToy

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ toys }) {
+function ToyContainer({ toys, deleteToy }) {
   return (
     <div id="toy-collection">
       {toys.map((toy) => (
